bf230a4d083c6c8023292ea32eb1f8b4768b8896
RahilLamisa <tasnimrahil2003002@gmail.com>
2025-03-02 01:00:03 +0600

    header part done

--- index.js
+++ index.js
@@ -7,5 +7,17 @@ document.getElementById('color').addEventListener('click',function(event){
     let randomColor = 'rgb('+red + ',' + green + ',' + blue + ')'
     document.body.style.backgroundColor=randomColor;
     // console.log(randomColor)
-    event.preventDefault()
+    event.preventDefault();
 })
+let today = new Date();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+let currentDay = days[today.getDay()]; 
+let currentMonth = months[today.getMonth()];
+let currentDate = today.getDate(); 
+let currentYear = today.getFullYear();
+
+let formattedDate = currentDay + ',' + '\n'+ ' '+ currentMonth + ' ' + currentDate + ' ' + currentYear ;
+document.getElementById("date").innerText = formattedDate;
+
